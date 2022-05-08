49cd491501df0bc033247f611ea6cfb73468c0af
Danil Anikin <90882787+m0rt4r@users.noreply.github.com>
2022-05-09 02:29:38 +1000

    Add files via upload

---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Bridge
+{   
+    // Levels.
+    class Level
+    {
+        public readonly string StringLevel;
+        public readonly int FieldFactor;
+
+        protected Level(string level, int fieldFactor)
+        {
+            StringLevel = level;
+            FieldFactor = fieldFactor;
+        }
+    }
+
+    class Easy: Level
+    {
+        public Easy(): base("Easy", 1) {}
+    }
+
+
+    class Normal: Level
+    {
+        public Normal(): base("Normal", 2) {}
+    }
+
+    class Hard: Level
+    {
+        public Hard(): base("Hard", 3) {}
+    }
+
+    // Enemies.
+    abstract class Enemy
+    {   
+        public readonly string Type;
+        public int Health;
+        public int Damage;
+        public int Speed;
+
+        protected Level Level;
+
+        protected Enemy(string type, Level level, int health, int damage, int speed)
+        {   
+            Type = type;
+            Level = level;
+            Health = health * level.FieldFactor;
+            Damage = damage  * level.FieldFactor;
+            Speed = speed  * level.FieldFactor;
+        }
+
+        public void ShowFields()
+        {
+            Console.WriteLine("Enemy type: " + Type + "\nHis level: " + Level.StringLevel + "\nHis Health: " + Health + "\nHis Daamge: " + Damage + "\nHis Speed: " + Speed + "\n");
+        }
+    }
+
+    class Zombie: Enemy
+    {
+        public Zombie(Level level): base("Zombie", level, 50, 3, 1){}
+    }
+
+    class Skeleton: Enemy
+    {   
+        public Skeleton(Level level): base("Skeleton", level, 80, 5, 2){}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Skeleton SkeletonFirst = new Skeleton(new Easy());
+            Skeleton SkeletonSecond = new Skeleton(new Normal());
+            Skeleton SkeletonThird = new Skeleton(new Hard());
+            Zombie ZombieFirst = new Zombie(new Easy());
+            Zombie ZombieSecond = new Zombie(new Hard());
+
+            SkeletonFirst.ShowFields();
+            SkeletonSecond.ShowFields();
+            SkeletonThird.ShowFields();
+            ZombieFirst.ShowFields();
+            ZombieSecond.ShowFields();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Prototype
+{   
+    public class Enemy
+    {
+        public int Health;
+        public int Damage;
+        public int Speed;
+
+        public Enemy(int health, int damage, int speed)
+        {
+            Health = health;
+            Damage = damage;
+            Speed = speed;
+        }
+
+        public Enemy CloneEnemy()
+        {
+            return new Enemy(Health, Damage, Speed);
+        }
+
+        public void ShowFields()
+        {
+            Console.WriteLine("Health: " + Health + "\nDamage: " + Damage + "\nSpeed: " + Speed + "\n");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy enemyOne = new Enemy(100, 10, 10);
+            Enemy enemyTwo = enemyOne.CloneEnemy();
+            
+            enemyOne.Health = 150;
+
+            Console.WriteLine("EnemyOne:");
+            enemyOne.ShowFields();
+
+            Console.WriteLine("EnemyTwo:");
+            enemyTwo.ShowFields();
+            
+
+            Console.ReadLine();
+        }
+    }
+}
